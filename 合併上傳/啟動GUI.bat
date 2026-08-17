4ba0b6d0316664430bfd09bd20b252408dc20b35
hub-google <250279659+hub-google@users.noreply.github.com>
2026-08-17 10:44:49 +0800

    Deploy all workspace changes

--- 合併上傳/啟動GUI.bat
+++ 合併上傳/啟動GUI.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 chcp 65001 >nul
 cd /d "%~dp0.."
 set "PYTHON_EXE=C:\DevTools\Python312\python.exe"
@@ -7,3 +8,4 @@ set "PYTHON_EXE=python"
 :run
 "%PYTHON_EXE%" "合併上傳\gui.py"
 if errorlevel 1 pause
+endlocal
